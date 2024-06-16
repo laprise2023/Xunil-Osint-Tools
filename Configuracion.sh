119b4f5421eab5070d05c2335c008518404998df
laprise2023 <154926868+laprise2023@users.noreply.github.com>
2024-06-16 20:18:19 +0200

    Add files via upload

--- Configuracion.sh
+++ Configuracion.sh
@@ -25,13 +25,12 @@ echo "$NARANJA
 
 echo "$AZUL1
 	╔══════════════════════════════════════════════════════════════════════════╗
-	║     Este script convertirá tu SO Debian en una distribución dedicada     ║
-	║  a la obtención de información en fuentes abiertas, realizará las        ║
-	║  siguientes acciones:                                                    ║
-	║       1. Instalará aplicaciones OSINT para la recoleción de información  ║
-	║          en fuentes abiertas.                                            ║
-	║       2. Personalizará el menú de escritorio para tener las              ║
-	║          aplicaciones accesibles                                         ║
+	║     Este script instala unos programas basico con fin de mostrar         ║
+	║  la forma de como se puede programar un script para realizar             ║
+	║  cualquier instalación con el fin de personalizar un entorno de trabajo  ║
+	║    1. Instalará actualizaciones necesarias para un buen funcionamiento   ║
+	║    2. Instalará un programa de obterncion de informaciónlas              ║
+	║    3. Colocara los inconos en el escritorio principal                    ║
 	╚══════════════════════════════════════════════════════════════════════════╝"
 }
 
@@ -69,6 +68,32 @@ cabecera
 echo  "$RC"
 echo "$AZUL3
 	      ╔═════════════════════════════════════════════════════╗
+	      ║             Instalando EyeWitness                   ║
+	      ╚═════════════════════════════════════════════════════╝"
+echo  "$RC"
+echo  "$RC"
+
+cd $HOME/XunilTools/
+sudo git clone https://github.com/RedSiege/EyeWitness.git
+cd EyeWitness/Python/setup
+sudo sh ./setup.sh
+cd $HOME/XunilTools/EyeWitness/Python
+python3 -m pip install --break-system-packages fuzzywuzzy
+python3 -m pip install --break-system-packages selenium==4.9.1
+python3 -m pip install --break-system-packages python-Levenshtein
+python3 -m pip install --break-system-packages pyvirtualdisplay
+python3 -m pip install --break-system-packages netaddr
+sudo chmod +x EyeWitness.py
+echo  "$RC"
+echo  "$VERDE EyeWitness instalado $RC"
+echo  "$RC"
+read opc
+echo e "$RC"
+
+cabecera
+echo  "$RC"
+echo "$AZUL3
+	      ╔═════════════════════════════════════════════════════╗
 	      ║             Instalando Maltego                      ║
 	      ╚═════════════════════════════════════════════════════╝"
 echo  "$RC"
@@ -84,7 +109,6 @@ echo  "$VERDE Maltego instalado $RC"
 echo  "$RC"
 read opc
 echo e "$RC"
-
 cabecera
 echo  "$RC"
 echo "$AZUL3
@@ -95,6 +119,21 @@ echo  "$RC"
 echo  "$RC"
 cd ~/XunilTools/instalacion
 sudo dpkg -i webapp-manager_1.3.4_all.deb
+echo  "$RC"
+echo  "$RC"
+echo  "$VERDE Webapp-Manager instalado $RC"
+echo  "$RC"
+read opc
+echo e "$RC"
+cabecera
+echo  "$RC"
+echo "$AZUL3
+	      ╔═════════════════════════════════════════════════════╗
+	      ║          Colocando iconos en escritorio             ║
+	      ╚═════════════════════════════════════════════════════╝"
+echo  "$RC"
+echo  "$RC"
+sudo cp /home/xunil/XunilTools/escritorio/* /home/xunil/Escritorio
 echo -e "$COLOROFF"
 sudo apt autoremove -y --purge
 sudo apt clean -y
@@ -102,8 +141,7 @@ sudo journalctl --vacuum-time=3d
 sudo find /var/log/ -type f -exec cp /dev/null {} \;
 rm -rf ~/.cache/*
 cat /dev/null > ~/.bash_history
-
-
+cabecera
 echo -e "$AZUL3 A continuación se reiniciará el sistema para aplicar los últimos cambios"
 echo -e "$NARANJA Pulsa intro para continuar"
 read -p ""
