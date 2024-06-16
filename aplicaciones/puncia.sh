0bc185a73fe77663eacf893c27d38fd027d646e0
laprise2023 <154926868+laprise2023@users.noreply.github.com>
2024-06-16 22:15:40 +0200

    Update puncia.sh

--- aplicaciones/puncia.sh
+++ aplicaciones/puncia.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #Luis Fernández
 
+# Se posciona sobre el directorio donde se encuentra el programa
+# que deseamos ejecutar
 cd $HOME/XunilTools/puncia
+
+# Orden de ejecución del programa
 puncia
+
+# Mantener la ventana Terminal abierta
 $SHELL
